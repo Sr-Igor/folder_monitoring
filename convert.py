df68d500c49a7ddff601355ccb3647455738ba23
Igor Rezende <sr.igor.dev@gmail.com>
2024-06-07 20:04:11 -0300

    FIX: remove warn with max size

--- convert.py
+++ convert.py
@@ -6,6 +6,7 @@ import logging
 import os
 import uuid
 from typing import Any
+import warnings
 import psd_tools
 import psycopg2
 from dotenv import load_dotenv
@@ -19,6 +20,9 @@ DB_URL = os.getenv("DB_URL")
 # Logger configuration
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+# Ignore warnings
+warnings.filterwarnings("ignore", category=Image.DecompressionBombWarning)
 
 
 def preview(
